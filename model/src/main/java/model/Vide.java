a8320fa6a33c2590eecf475a45d0a900a0ab29b6
Lucas <lucasbieger1@gmail.com>
2019-05-27 17:00:30 +0200

    Element

---
+++ model/src/main/java/model/Vide.java
@@ -0,0 +1,23 @@
+package model;
+
+import fr.exia.insanevehicles.model.element.Permeability;
+import fr.exia.insanevehicles.model.element.Sprite;
+
+/**
+ * <h1>The DitchLeft Class.</h1>
+ *
+ * @author Jade
+ * @version 0.2
+ */
+public class Vide extends MotionlessElement {
+
+    /** The Constant SPRITE. */
+    private static final Sprite SPRITE = new Sprite('B', "Vide.png");
+
+    /**
+     * Instantiates a new ditchLeft.
+     */
+    public Vide() {
+        super(SPRITE, Permeability.BLOCKING);
+    }
+}
