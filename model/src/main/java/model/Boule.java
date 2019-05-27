88ec609edce65cf24c0fc5aa6a6fd71d85cbc94b
Lucas <lucasbieger1@gmail.com>
2019-05-27 16:59:37 +0200

    Elément

---
+++ model/src/main/java/model/Boule.java
@@ -0,0 +1,23 @@
+package model;
+
+import fr.exia.insanevehicles.model.element.Permeability;
+import fr.exia.insanevehicles.model.element.Sprite;
+
+/**
+ * <h1>The DitchLeft Class.</h1>
+ *
+ * @author Jade
+ * @version 0.2
+ */
+class Boule extends MotionlessElement {
+
+    /** The Constant SPRITE. */
+    private static final Sprite SPRITE = new Sprite('R', "Roche.png");
+
+    /**
+     * Instantiates a new ditchLeft.
+     */
+    Boule() {
+        super(SPRITE, Permeability.BLOCKING);
+    }
+}
